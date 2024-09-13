61d2a7c79fedcfa4213b5e254c4d34cd909a465d
theanhnguyen3112 <anh.nguyenthe3112@gmail.com>
2024-09-13 17:24:50 +0700

    13/9/24-17:24

--- ur_moveit_config/launch/ur_moveit.launch.py
+++ ur_moveit_config/launch/ur_moveit.launch.py
@@ -270,6 +270,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "ur_type",
+            default_value="ur5",
             description="Type/series of used UR robot.",
             choices=["ur3", "ur3e", "ur5", "ur5e", "ur10", "ur10e", "ur16e", "ur20", "ur30"],
         )
